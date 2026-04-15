a15638f5710559ba81a65e0a7bbc05b7d5ec635a
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-15 12:44:47 -0700

    Update exp_factory.py

--- exp_factory.py
+++ exp_factory.py
@@ -5,22 +5,9 @@
 import random
 
 class ExpertFactory:
-  
+  #Creates the difficult enemies 
   def create_monster(self):
-    monster_type = random.choice(["Goblin", "Ogre", "Troll"])
-
-  if monster_type == "Goblin":
-    hp = random.randint(6,10)
-    dmg = random.randint(5,8)
-    return Goblin(hp,dmg)
-
-  elif monster_type == "Ogre":
-    hp = random.randint(8,12)
-    dmg = random.randint(6,10)
-    return Ogre(hp,dmg)
-
-  else:
-    hp = random.randint(10,14)
-    dmg = random.randint(8,12)
-    return Troll(hp,dmg)
-
+    # Randomly generate and returns a difficult enemy
+    enemies = [Goblin, Ogre, Troll]
+    chosen_enemy = random.choice(enemies)
+    return chosen_enemy()
