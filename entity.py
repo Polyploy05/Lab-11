1ceecbf302e16a4e8935605bd4e82de570b31a79
Polyploy05 <jakada005@gmail.com>
2026-04-15 13:09:13 -0700

    Update entity.py

--- entity.py
+++ entity.py
@@ -2,41 +2,29 @@
 Name: Daniel Puerto & Jacob Miranda
 Date: 4/15/26
 Group: 15
-Description: The basic constructor class for any entity. Allows for each entity to be given
-a name and hp, the ability to take damage, and a string representation. 
-Also has an abstract method for standardmelee attack
+Description: Constructor for the hero class. Inherits Entity and implements the melee 
+attack method. Also has a ranged attack method that is unique to the hero class.
 '''
 
 
-import abc
+import entity
+import random
 
-class Entity(abc.ABC):
-    def __init__(self, name, hp):
-        self._name = name
-        self._hp = hp
+class Hero(entity.Entity):
+    def __init__(self, name):
+        super().__init__(name, 25)
 
-    @property
-    def name(self):
-        return self._name
-    
-    @property
-    def hp(self):
-        return self._hp
-    
-    def take_damage(self, damage):
-        #Remove damage from hp. Prevents hp from going below 0
-        self._hp -= damage
-        if self._hp < 0:
-            self._hp = 0  
-
-    def __str__(self):
-        return f"{self._name} (HP: {self._hp})"
-    
-    @abc.abstractmethod
     def melee_attack(self, target):
-        pass
-
-
+        #Roll 2d6 for damage. Total damage is done to the enemy and the result is printed
+        damage = random.randint(1, 6) + random.randint(1, 6)
+        target.take_damage(damage)
+        return f"{self.name} attacks {target.name} for {damage} damage!"
+        
+    def ranged_attack(self, target):
+        #Roll 1d12 for damage. Total damage is done to the enemy and the result is printed
+        damage = random.randint(1, 12)
+        target.take_damage(damage)
+        return f"{self.name} shoots {target.name} for {damage} damage!"
 
 
 
