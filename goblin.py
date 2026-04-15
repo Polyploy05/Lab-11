331baf3de2e8fc12dcc99aaf870f080f4dae81c4
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-15 13:01:57 -0700

    Update goblin.py

--- goblin.py
+++ goblin.py
@@ -2,7 +2,7 @@
 Name: Daniel Puerto & Jacob Miranda
 Date: 4/15/26
 Group: 15
-Description:
+Description: Creates the goblin class that is used in the expert factory.
 '''
 
 
@@ -11,9 +11,11 @@ import entity
 import random
 
 class Goblin(entity.Entity):
+    #Creates the goblin entity with a random set of hp ranging from 6-10
     def __init__(self):
         super().__init__("Goblin", random.randint(6, 10))
-
+        
+    #Creates the attack that does damage to the hero
     def melee_attack(self, enemy):
         damage = random.randint(5, 8)
         enemy.take_damage(damage)
