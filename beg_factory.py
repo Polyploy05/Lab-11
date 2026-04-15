3f74ff24067a366dd3972286b56b05991be9d241
Polyploy05 <jakada005@gmail.com>
2026-04-15 12:44:48 -0700

    Update beg_factory.py

--- beg_factory.py
+++ beg_factory.py
@@ -1,16 +1,18 @@
 
 
 import random
-from enemy_factory import EnemyFactory
-from easy_goblin import EasyGoblin
-from easy_ogre import EasyOgre
-from easy_troll import EasyTroll
-
-
+import enemy_factory
+import easy_ogre
+import easy_goblin
+import easy_troll
 
 class BeginnerFactory(enemy_factory.EnemyFactory):
     def create_random_enemy(self):
-        enemies = [EasyGoblin, EasyOgre, EasyTroll]
-        chosen_enemy = random.choice(enemies)
-        return chosen_enemy
+        enemy_type = random.choice(['easy_ogre', 'easy_goblin', 'easy_troll'])
+        if enemy_type == 'easy_ogre':
+            return easy_ogre.EasyOgre()
+        elif enemy_type == 'easy_goblin':
+            return easy_goblin.EasyGoblin()
+        elif enemy_type == 'easy_troll':
+            return easy_troll.EasyTroll()
         
