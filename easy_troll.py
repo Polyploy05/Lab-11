5f8e74fd14817b7848610a103bee9eca88b892b5
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-15 12:56:54 -0700

    Update easy_troll.py

--- easy_troll.py
+++ easy_troll.py
@@ -2,7 +2,7 @@
 Name: Daniel Puerto & Jacob Miranda
 Date: 4/15/26
 Group: 15
-Description:
+Description: Creates the Troll, easy version, that is to be called when the player is in the beginner factory
 '''
 
 
@@ -10,10 +10,12 @@ import entity
 import random
 
 class EasyTroll(entity.Entity):
+    #Creates the easy troll entity
     def __init__(self):
         super().__init__("Easy Troll", random.randint(6, 9))
 
     def melee_attack(self, enemy):
+        #Creates the attack that does damage to the hero.
         damage = random.randint(5, 9)
         enemy.take_damage(damage)
         return f"{self.name} whacks {enemy.name} for {damage} damage!"
