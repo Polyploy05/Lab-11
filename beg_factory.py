c75e18a8c2bef3760999bd5ee228443d1c1c58d5
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-15 12:42:08 -0700

    Update beg_factory.py

--- beg_factory.py
+++ beg_factory.py
@@ -1,8 +1,16 @@
 
 
 import random
-import enemy_factory
+from enemy_factory import EnemyFactory
+from easy_goblin import EasyGoblin
+from easy_ogre import EasyOgre
+from easy_troll import EasyTroll
+
+
 
 class BeginnerFactory(enemy_factory.EnemyFactory):
-    def create_random_enemy(self, name, hp):
+    def create_random_enemy(self):
+        enemies = [EasyGoblin, EasyOgre, EasyTroll]
+        chosen_enemy = random.choice(enemies)
+        return chosen_enemy
         
